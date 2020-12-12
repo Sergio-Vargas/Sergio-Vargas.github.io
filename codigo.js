ddd38e83b23826cc040ac245b394583b3fa67cba
Sergio-Vargas <73031293+Sergio-Vargas@users.noreply.github.com>
2020-12-11 22:57:51 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,19 @@
+function Noche(){
+	console.log("hola")
+	document.body.style.backgroundColor= "RGB(46, 80, 165)";
+	celda = document.getElementById("tabla");
+	celda.style.backgroundColor="RGB(230, 232, 232)";
+	nav=document.getElementById("nav");
+	nav.style.backgroundColor="gray"
+}
+function sendMessage(){
+msj=document.getElementById("msj").value;
+if (msj=="") {
+	alert("Esta Vacío");
+}
+else{
+	var par = document.getElementById("p");
+	var newContentName=document.createTextNode(""+msj);
+	par.appendChild(newContentName);
+}
+}
